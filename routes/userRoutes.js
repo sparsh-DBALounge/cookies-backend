45106cf000932b2faad6c4d3f3b27560ec8663cc
sparsh-DBALounge <sparsh.kadian@dbalounge.com>
2024-09-12 19:59:04 +0530

    randomPost function to test getting cookies from client

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup, login } from '../controllers/userController.js';
+import { signup, login, randomPost } from '../controllers/userController.js';
 import { saveUser } from '../middleware/userAuth.js';
 
 const router = express.Router();
 
 router.post('/signup', saveUser, signup);
 router.post('/login', login);
+router.post('/random', randomPost);
 
 export default router;
